8da7b7126cec71e79b8a84b90a364b3fa144d87e
lavallone <lavalle.1838492@studenti.uniroma1.it>
2023-07-19 17:55:14 +0200

    add planner

---
+++ planning/planner.py
@@ -0,0 +1,54 @@
+from unified_planning.shortcuts import *
+
+Cell = UserType('Cell')
+Bin = UserType('Bin')
+Agent = UserType('Agent')
+
+## FLUENTS definition
+agent_at = unified_planning.model.Fluent('agent_at', BoolType(), robot=Agent, position=Cell)
+is_agent = unified_planning.model.Fluent('is_agent', BoolType(), robot=Agent)
+bin_at = unified_planning.model.Fluent('bin_at', BoolType(), bin=Bin, position=Cell)
+is_bin = unified_planning.model.Fluent('is_bin', BoolType(), bin=Bin)
+adj = unified_planning.model.Fluent('adj', BoolType(), from_position=Cell, to_position=Cell)
+throw = unified_planning.model.Fluent('throw', BoolType(), bin=Bin)
+# numerical fluents for the level of the bins
+level = unified_planning.model.Fluent("level", IntType(0,3), bin=Bin)
+
+## ACTIONS definition
+# move
+move = unified_planning.model.InstantaneousAction('move', robot=Agent, from_position=Cell, to_position=Cell)
+robot = move.parameter('robot')
+from_position = move.parameter('from_position')
+to_position = move.parameter('to_position')
+move.add_precondition(adj(from_position, to_position))
+move.add_precondition(is_agent(robot))
+move.add_precondition(agent_at(robot, from_position))
+move.add_effect(agent_at(robot, from_position), False)
+move.add_effect(agent_at(robot, to_position), True)
+
+# throw
+throw_bin = unified_planning.model.InstantaneousAction('throw', robot=Agent, bin=Bin, position=Cell)
+robot = move.parameter('robot')
+bin_ = move.parameter('bin')
+position = move.parameter('position')
+throw_bin.add_precondition(is_agent(robot))
+throw_bin.add_precondition(agent_at(robot, position))
+throw_bin.add_precondition(is_agent(bin_))
+throw_bin.add_precondition(agent_at(bin_, position))
+# how to define the inequality of the level?
+throw_bin.add_precondition(LT(level(bin_), 3))
+throw_bin.add_effect(throw(bin_), True)
+
+## PROBLEM definition
+problem = unified_planning.model.Problem('school')
+problem.add_fluent(agent_at)
+problem.add_fluent(is_agent)
+problem.add_fluent(bin_at)
+problem.add_fluent(is_bin)
+problem.add_fluent(adj)
+problem.add_fluent(throw)
+problem.add_fluent(level)
+problem.add_action(move)
+problem.add_action(throw_bin)
+
+# define OBJECTS
