80b8be08ccc105058ea6bd1ce1b05fd925cefd90
Garbetta <garbetta.1785139@studenti.uniroma1.it>
2023-07-20 14:47:55 +0200

    upd imgs and loops

--- newdemo/scripts/hri_prova.py
+++ newdemo/scripts/hri_prova.py
@@ -21,7 +21,10 @@ def i1():
                'paper':'img/trash/bin_paper.png', 
                'compost':'img/trash/bin_compost.png', 
                'waste':'img/trash/bin_waste.png'}
-    
+
+    def face_detection():
+        print('faceDetection')
+
     def giochino():
         dir_path = 'img/play_trash/'
         dict_trash = {}
@@ -51,27 +54,73 @@ def i1():
 
     debug = True
     im.init()   #azione che va lanciata per prima (dopo le varie strutture e inizializzazioni - nel while true)
-    someone = False
+    
     finished = False
-    if debug:
-        p = im.robot.sensorvalue()
-        print(p)
-    while True:
-        while(not someone): 
-            ask = im.ask(None, timeout = 10)
-            someone = True 
+    someone_people = ''
+    list_peoples = []
 
-        eta = 'timeout' ##
-        while(eta == 'timeout'):
-            eta = im.ask('welcome', timeout=15)
-            if(eta == 'elementary'):
+    while True:
+        finished = False
+        someone = False
+        while(not someone): 
+            ask = im.ask(None)
+            sensor = im.robot.sensorvalue()     #--> 0laser, 1 sonarfront, 2sonarback, 3head, 4hand, 4hand
+            if debug: print(sensor)
+            '''if sensor[2] < 1.5:
+                time.sleep(5) 
+                if sensor[2] < 1.5: 
+                    im.executeModality('TTS', 'Do you need help? Come here!')
+                    time.sleep(3)
+                    if sensor[1] < 1.5:
+                        #start interaction
+                        face_detection()
+                        #someone = True
+                        #continue
+                else: print('I am alone!')
+            if sensor[1] < 1.5:
+                time.sleep(3)
+                if sensor[1] < 1.5: 
+                    face_detection()
+                    #someone = True
+                    #continue'''
+
+
+            someone = True 
+            '''if someone == True
+            someone_people = 'Piero'
+            '''
+
+
+        #TODO
+        #if someone_people not in list_peoples: 
+            # chiedo la scuola, faccio i controlli e aggiungo le info etc
+            # else :leggo info list people, setto il profilo giusto e vado diretto a riga ---  activity = im.ask('activity') (farla fuori dall if)
+
+        age = 'timeout' ##
+        face_age = ['elementary', 'middle']
+        while(age == 'timeout'):
+            age = im.ask('welcome', timeout=15)
+            if(age == 'elementary'):
                 im.profile[0] = 'elementary'
                 if debug: print(im.profile)
                 
-            elif(eta == 'middle'):
-                im .profile[0] = 'middle'
+            elif(age == 'middle'):
+                im.profile[0] = 'middle'
                 if debug: print(im.profile)
-            
+                
+                if face_age[0] != 'middle':
+                    im.executeModality('TEXT', 'Maybe you have selected the wrong choice! Are not you from the elementary school?')
+                    im.executeModality('TTS', 'Maybe you have selected the wrong choice! Are not you from the elementary school?')
+                    im.executeModality('BUTTONS', [('yes', 'YES'), ('no', 'NO')])
+                    ans_age = im.ask(None, timeout = 15)
+                    if ans_age == 'yes':
+                        im.profile[0] = 'elementary'
+                        if debug: print(im.profile)
+        #else: ####    
+        #im.profile[0] = someone_people.scholl
+        # im.executeModality('TEXT', 'Hi someone_people.name, what do you want to do?' )
+        # im.executeModality('TTS', 'Hi someone_people.name, what do you want to do?')
+        #
         activity = im.ask('activity')
             
         if(activity == 'recycle'):
@@ -94,7 +143,7 @@ def i1():
             im.executeModality('IMAGE', garbage_bin[garbage_class])
             time.sleep(5)
             im.executeModality('TEXT', "I'm searching for the best path to the "+garbage_class+" bin")
-            im.executeModality('IMAGE', 'img/trash/loading-25.gif')      
+            im.executeModality('IMAGE', 'img/loading/loading-25.gif')      
             #TODO              
             #fare pezzo del caricamento della mappa con il percorso
             time.sleep(10)
@@ -131,10 +180,15 @@ def i1():
             st = im.ask('play_welcome', timeout = 15)
             if st == 'start':
                 finished = giochino()
-            im.executeModality('TEXT', 'Waiting...')
+                im.executeModality('TEXT', 'Waiting...')
+            else:
+                finished = True
+                im.executeModality('TEXT', 'Exiting...')
             time.sleep(10)
-
-        if finished: im.execute('goodbye')
+            
+        if finished: 
+            feed = im.ask('feedback', timeout = 20)
+            im.execute('goodbye')
 
 
         im.init()           #ritorno alla schermata iniziale
