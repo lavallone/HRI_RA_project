b637280515f92f9fad0e4ca9d673f644685d87c0
lavallone <lavalle.1838492@studenti.uniroma1.it>
2023-07-31 16:42:22 +0200

    fix bugs

--- docker/init.bash
+++ docker/init.bash
@@ -4,15 +4,15 @@ SESSION=init
 
 tmux -2 new-session -d -s $SESSION
 
-tmux rename-window -t $SESSION:0 'human_simulation'
-#tmux new-window -t $SESSION:1 -n 'naoqi'
-tmux new-window -t $SESSION:1 -n 'pepper_interaction'
-tmux new-window -t $SESSION:2 -n 'modim_server'
+tmux rename-window -t $SESSION:0 'naoqi'
+tmux new-window -t $SESSION:1 -n 'human_simulation'
+tmux new-window -t $SESSION:2 -n 'pepper_interaction'
+tmux new-window -t $SESSION:3 -n 'modim_server'
 
-tmux send-keys -t $SESSION:0 "cd ~/playground/scripts" C-m
-#tmux send-keys -t $SESSION:1 "cd /opt/Aldebaran/naoqi-sdk-2.5.5.5-linux64" C-m
+tmux send-keys -t $SESSION:0 "cd /opt/Aldebaran/naoqi-sdk-2.5.5.5-linux64" C-m
 tmux send-keys -t $SESSION:1 "cd ~/playground/scripts" C-m
-tmux send-keys -t $SESSION:2 "cd src/modim/src/GUI" C-m
+tmux send-keys -t $SESSION:2 "cd ~/playground/scripts" C-m
+tmux send-keys -t $SESSION:3 "cd src/modim/src/GUI" C-m
 
 
 while [ 1 ]; do
