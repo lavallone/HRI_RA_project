bffba0270e67ff100ef95fd38dce9f590dc1c3cf
Garbetta <garbetta.1785139@studenti.uniroma1.it>
2023-08-03 14:45:57 +0200

    upd ale int-news

--- pepper_interaction/scripts/human_interaction_fake.py
+++ pepper_interaction/scripts/human_interaction_fake.py
@@ -14,7 +14,6 @@ import select
 from threading import Thread
 
 
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-pip", type=str, default=os.environ['PEPPER_IP'],
@@ -44,7 +43,6 @@ def main():
     fakeASRevent = 'FakeRobot/ASRevent'
     fakeASRtimekey = 'FakeRobot/ASRtime'
 
-
     memkey = {
     'SonarFront': 'Device/SubDeviceList/Platform/Front/Sonar/Sensor/Value',
     'SonarBack':  'Device/SubDeviceList/Platform/Back/Sonar/Sensor/Value', 
@@ -54,29 +52,85 @@ def main():
     }
 
     memkey_transl = {
-        1:'SonarFront',
-        2: 'SonarBack',
-        3: 'HeadMiddle',
-        4: 'LHand',
-        5: 'RHand'
+        'sonarf':'SonarFront',
+        'sonarb': 'SonarBack',
+        'head': 'HeadMiddle',
+        'lhand': 'LHand',
+        'rhand': 'RHand'
     }
     sent = 'START'
 
     #print('To simulate the interaction of the human with Pepper you can write directly what do you want to tell and for the physical reaction'+
     # ' you ahve to use this template: nofsensor_valueofsensor')
 
+    '''
+    def is_number(s):
+        if s.isdigit(): return True
+        else:
+            try:
+                float(s)
+                return True
+            except ValueError:
+                return False
+
     while sent != 'STOP':
         sent = raw_input('Human: ')
 
         sent = sent.split('-')
         
-        if sent[0].isdigit():              #if is integer or float I have to simulate a pressure or a presence 
-                                           # I use a a number of sensorValue to inidcate how sensor I have to imposed 
-            sensor_class = int(sent[0])
-            sensor_value = float(sent[1])
-            
-            if sensor_class in [1, 2]:      #== 1 or sensor_class == 2:   #sonar front / sonar back
-                print('Type Sensor Input = %d, Sensor Value = %f' %(sensor_class, sensor_value))
+        if len(sent) > 1:
+            print(sent, (type(sent[0]), type(sent[1])))
+            if is_number(sent[0]) and is_number(sent[1]):              #if is integer or float I have to simulate a pressure or a presence 
+                                            # I use a a number of sensorValue to inidcate how sensor I have to imposed 
+                sensor_class = int(sent[0])
+                sensor_value = float(sent[1])
+                
+                if sensor_class in [1, 2]:      #== 1 or sensor_class == 2:   #sonar front / sonar back
+                    print('Type Sensor Input = %d, Sensor Value = %f' %(sensor_class, sensor_value))
+                    try:
+                        mkey = memkey[memkey_transl[sensor_class]]
+                        print("Sonar %s = %f" %(mkey, sensor_value))
+                        memory_service.insertData(mkey,sensor_value)
+                        #time.sleep(args.duration)                      #it is necessary only if we want simulate the value for a determinated time 
+                        #memory_service.insertData(mkey,0.0)            #vedere se va bene
+                        print(memory_service.getData(mkey,sensor_value))
+                    except:
+                        print("ERROR: Sensor %s unknown" %args.sensor)
+                
+                else:       #if sensor_class == 3 or sensor_class == 4 or sensor_class == 5:
+                    try:
+                        mkey = memkey[memkey_transl[sensor_class]]
+                        print("Touching %s ..." %mkey)
+                        #print("Robot Say: Hey, don't touch me!")
+                        memory_service.insertData(mkey,1.0)
+                        time.sleep(3)               #touch interaction duration 
+                        memory_service.insertData(mkey,0.0)
+                        print("Touching %s ... done" %mkey)
+                    except:
+                        print("ERROR: Sensor %s unknown" %args.sensor)
+                
+                continue
+                
+        sent = sent[0]
+        tm = int(time.time())
+        memory_service.raiseEvent(fakeASRevent, sent)
+        memory_service.insertData(fakeASRkey, sent)
+        memory_service.insertData(fakeASRtimekey, tm)
+        print("Human Say: '%s' " %(sent))
+    '''
+
+    while sent != 'STOP':
+        
+        sent = raw_input('Human: ')
+
+        split = sent.split(' ')
+
+        if split[0] in     memkey_transl.keys():
+            sensor_class = split[0]
+            sensor_value = float(split[1])
+
+            if sensor_class in ['sonarf', 'sonarb']:      #== 1 or sensor_class == 2:   #sonar front / sonar back
+                print('Type Sensor Input = %s, Sensor Value = %f' %(sensor_class, sensor_value))
                 try:
                     mkey = memkey[memkey_transl[sensor_class]]
                     print("Sonar %s = %f" %(mkey, sensor_value))
@@ -87,25 +141,23 @@ def main():
                 except:
                     print("ERROR: Sensor %s unknown" %args.sensor)
             
-            else:       #if sensor_class == 3 or sensor_class == 4 or sensor_class == 5:
+            else:       #if sensor_class in ['HeadMiddle', 'LHans', 'RHand']:
                 try:
                     mkey = memkey[memkey_transl[sensor_class]]
                     print("Touching %s ..." %mkey)
-                    #print("Robot Say: Hey, don't touch me!")
                     memory_service.insertData(mkey,1.0)
                     time.sleep(3)               #touch interaction duration 
                     memory_service.insertData(mkey,0.0)
                     print("Touching %s ... done" %mkey)
                 except:
                     print("ERROR: Sensor %s unknown" %args.sensor)
-
         else:
-            sent = sent[0]
             tm = int(time.time())
             memory_service.raiseEvent(fakeASRevent, sent)
             memory_service.insertData(fakeASRkey, sent)
             memory_service.insertData(fakeASRtimekey, tm)
             print("Human Say: '%s' " %(sent))
+
 
 if __name__ == "__main__":
     main()
--- pepper_interaction/scripts/main_behaviour.py
+++ pepper_interaction/scripts/main_behaviour.py
@@ -14,6 +14,7 @@ except Exception as e:
 import ws_client
 from ws_client import *
 
+sys.path.append(os.getenv('PEPPER_TOOLS_HOME')+'/cmd_server')
 
 def behaviour():
     
@@ -28,15 +29,7 @@ def behaviour():
                  'compost':['apple', 'banana', 'fruit', 'bread', 'food', 'sandwich']}
 
     DEBUG = True
-    im.init()
-    
-    fd_w = open('/home/robot/playground/info_people.txt', 'a')
-    fd_r = open('/home/robot/playground/info_people.txt', 'r')
-    
-
-    #print(im.robot.alive)
-    #im.robot.setAlive(True)
-    #print(im.robot.isConnected)
+    im.init()   
     
     finished = False
     p_back = False          #True if there is a people behind pepper
@@ -45,6 +38,7 @@ def behaviour():
     def onTouched(value):
         print('IN ON-TOUCHED', value)
         im.executeModality('TTS', 'Hey Do not touch me!')
+        time.sleep(1)
     
     anyTouch = im.robot.memory_service.subscriber("TouchChanged")
 
@@ -56,10 +50,11 @@ def behaviour():
     #print(im.robot.memory_service.getEventList())
     
     while True:
+
         finished = False
         someone = False
 
-        read = fd_r.read()
+        read = open('/home/robot/playground/info_people.txt', 'r').read()
         if DEBUG: print('La READ:', read)
         list_peoples = []
         list_peoples = read.split('\n')
@@ -82,14 +77,16 @@ def behaviour():
                     while (p_back):
                         sensor = im.robot.sensorvalue()
                         if DEBUG: print(sensor)
+                        time.sleep(5)
                         #im.executeModality('IMAGE', 'img/pepper.back.jpg')
                         im.executeModality('TTS', 'Do you need help? Come here!')
                         if sensor[1] > 0.0 and sensor[1] < 1.5:
                             someone = True
                             print('I HAVE DETECT ONE PEOPLE FOR MORE THAN 5 SECONDS')
+                            p_back = False
                             #start interaction
                             #continue
-                        elif sensor[2] > 1.5: p_back = False
+                        elif sensor[2]== 0.0 or  sensor[2] > 1.5: p_back = False
                 else: print('I am alone!')
             if sensor[1] > 0.0 and sensor[1] < 1.5:
                 time.sleep(3)
@@ -97,62 +94,77 @@ def behaviour():
                     someone = True
                     #continue
 
-        im.robot.stopSensorMonitor()    
+        im.robot.stopSensorMonitor()
 
         #print('VOCA', im.vocabulary)
 
         if someone:                 #start interaction
-            im.ask('presentation', timeout = 10)
+            im.profile[1] = 'a'
+            im.execute('presentation')
+            time.sleep(10)
             new_user = im.robot.memory_service.getData('FakeRobot/ASR')
-            #new_user = 'leo lavalle 10'
-            #print(new_user)
-            new_user_s = new_user.split(' ')
-            #print(new_user_s)
 
-            #lo metto in un while??
-            if new_user not in list_peoples:
-                fd_w.write(new_user+'\n')
+            print(new_user)
+            new_user_s = new_user.split(' ')
+            print(new_user_s)
+
+            #da mettere nell'if new?
+            im.executeModality('TEXT', 'How old are you?')
+            im.executeModality('TTS', 'and, How old are you"')
+            time.sleep(10)
+            new_user_age = im.robot.memory_service.getData('FakeRobot/ASR')
+            print(new_user_age)
+           
+            for elem in list_peoples:
+                elem = elem.split(' ')
+                print(elem)
+                if elem[0] == new_user_s[0] and elem[1] == new_user_s[1]: 
+                    new = False
+                    print(elem[2])
+                    old_user_school = elem[2]
+                    break
+                else: new = True 
+            if new:
+                open('/home/robot/playground/info_people.txt', 'a').write(new_user)
                 age = 'timeout'
-                while(age == 'timeout'):
-                    age = im.ask('welcome', timeout=15)
-                    if(age == 'elementary'):
-                        im.profile[0] = 'elementary'
-                        if DEBUG: print(im.profile)
-                        
-                    elif(age == 'middle'):
-                        im.profile[0] = 'middle'
-                        if DEBUG: print(im.profile)
-                        #modifiare interazione con bambino elementari, vedi foglio (1)
-                        #if int(new_user[2]) < int('11'):
-                            #im.executeModality('TEXT', 'Maybe you have selected the wrong choice! Are not you from the elementary school?')
-                            #im.executeModality('TTS', 'Maybe you have selected the wrong choice! Are not you from the elementary school?')
-                            #im.executeModality('BUTTONS', [('yes', 'YES'), ('no', 'NO')])
-                            #ans_age = im.ask(None, timeout = 15) #cambaire ask
-                            #if ans_age == 'yes':
-                                #im.profile[0] = 'elementary'
-                                
-                                #if DEBUG: print(im.profile)
+                im.profile[1] = 'new'
+                #####
+                if int(new_user_age) < 11:
+                    im.profile[0] = 'elementary'
+                    while(age == 'timeout'):
+                        age = im.ask('welcome', timeout=15)      
+                        if age == 'elementary': open('/home/robot/playground/info_people.txt', 'a').write(' elementary\n')
+                        if(age == 'middle'):
+                            open('/home/robot/playground/info_people.txt', 'a').write(' middle\n')
+                            im.profile[0] = 'middle'
+                            if DEBUG: print(im.profile)
                     #MODIFY PROFILE - lasciare solo vocale
-                    #im.executeModality('TEXT', 'Hi '+new_user[0]+' 'new_user[1]+', what do you want to do?')
-                    #im.executeModality('TTS', 'Hi '+new_user[0]+' 'new_user[1]+', what do you want to do?')
+
+                    #im.executeModality('TEXT', 'Hi '+new_user_s[0]+' '+new_user_s[1]+', what do you want to do?')
+                    im.executeModality('TTS', 'Hi '+new_user_s[0]+' '+new_user_s[1]+'.')
+                    im.execute('presentation')
+                    time.sleep(5)
                     #inserire parte di presentation su quello che fa
                     #add gesture di saluto
-
-            else:
-                if int(new_user_s[2]) < int('11'): im.profile[0] = 'elementary'
                 else: im.profile[0] = 'middle'
+            else:
+                if old_user_school ==  'elementary': im.profile[0] = 'elementary'
+                else: im.profile[0] = 'middle'
+                im.profile[1] = 'old'
                 #MODIFY PROFILE - lasciare solo vocale
                 #im.executeModality('TEXT', 'Welcom back '+new_user[0]+' 'new_user[1]+', what do you want to do?')
-                #im.executeModality('TTS', 'Welcom back '+new_user[0]+' 'new_user[1]+', what do you want to do?')
+                im.executeModality('TTS', 'Welcome back '+new_user_s[0]+' '+new_user_s[1]+'.')
+                im.execute('presentation')
+                time.sleep(5)
                 #vedi testo
-
+        
         activity = im.ask('activity', timeout = 25)
             
         if(activity == 'recycle'):
             im.profile[3] = 'recycle'
             im.ask('recycle', timeout = 15) #only asr answer
             ans_rec = im.robot.memory_service.getData('FakeRobot/ASR')
-            print('ANSSSSS RECCC', ans_rec)
+            print('ANS REC', ans_rec)
             ans_rec = ans_rec.split(' ')
             garbage_class = '' 
             
@@ -162,28 +174,27 @@ def behaviour():
                         garbage_class = k
                         break
 
-            print('GARBAGE KEy', garbage_class)
+            print('GARBAGE KEY', garbage_class)
             
             if(garbage_class == ''):
-                if(im.profile[0] == 'elementary'):
-                    im.executeModality('IMAGE', 'imgs/trash/walle.jpg')
+                if(im.profile[0] == 'elementary'): im.executeModality('IMAGE', 'imgs/trash/walle.jpg')
                 im.executeModality('TEXT', 'Let me see the object') #cambiare il text aggiungere tts
-                time.sleep(5)
-
                 img_path = "data/users_imgs/"+str(random.randint(1, 20))+".jpg"
                 garbage_class, ris_img_path= im.detect_garbage(img_path)
+                #ris_img_path = "../../../../playground/vision/garbage_detection/"+ris_img_path
+                time.sleep(5)
                 im.executeModality('IMAGE', 'vision/garbage_detection/'+ris_img_path)
                 time.sleep(5)
             
             im.executeModality('IMAGE', 'imgs/map/map.png')
             im.executeModality('TTS', 'This is the map of the school, these are the available bins in which you can throw your object')
-            
-            map_goals_path, map_bestpath_path =im.shortest_path(garbage_class)
+            time.sleep(5)
+            map_goals_path, map_bestpath_path = im.shortest_path(garbage_class)
             map_goals_path = map_goals_path[3:]
             map_bestpath_path = map_bestpath_path[3:]
 
-
             im.executeModality('IMAGE', map_goals_path)
+            time.sleep(5)
             im.executeModality('TTS', 'Now I will show you the fastest path to reach the '+garbage_class+' bin')
             im.executeModality('IMAGE', 'imgs/loading/loading-25.gif')   
             time.sleep(5)
@@ -204,20 +215,36 @@ def behaviour():
                                             ["plastic", "Plastic in oceans", "https://oceanliteracy.unesco.org/plastic-pollution-ocean/"],
                                             ["paper", "Paper recycling" , "https://www.twosides.info/paper-packaging-is-recycled-more-than-any-other-material"]
                                             ])
-            
-            finished = False
             while not finished:
-                time.sleep(10)
+                time.sllep(10)
+                im.executeModality('TTS', 'Have you finished reading?')
                 # qui a loop ogni tot secondi il robot chiede allo user se ha finito...
                 # appena gli risponde di si --> si setta finished=True ed esce dal loop!
+                time.sleep(10)
+                ans_news = im.robot.memory_service.getData('FakeRobot/ASR')
+                time.sleep(5)
+                if ans_news == 'yes': finished = True
                 
-                finished = True
-                
+
             im.display.remove_buttons()
             im.display.display_image("", place='left')
             im.display.display_image("")
             im.display.display_image("", place='right')
             im.display.display_text("", "default")
+            '''
+            article = im.ask('news')
+            im.executeModality('IMAGE', '')
+            #button = im.executeModality('BUTTONS', [('exit', 'EXIT'), ('back', 'BACK')])
+            article = int(article)-1
+            
+            pages = ['microplastics', 'pollution', 'fashion', 'recycled']
+            print(pages)
+            button = im.ask(pages[article], timeout = 25)        
+
+            if button == 'exit': finished = True
+            elif button == 'back': finished = False
+            elif button == 'timeout':finished = True
+            '''
 
         elif(activity == 'play'):
             im.profile[3] = 'play'
@@ -229,17 +256,17 @@ def behaviour():
             else:
                 finished = True
                 im.executeModality('TEXT', 'Exiting...')
-            time.sleep(10)
+            time.sleep(5)
             
-        if finished:
+        if finished: 
+            im.execute('goodbye')
             feed = im.ask('feedback', timeout = 20)
             # robot deve avere una reazione diversa a seconda dello score rating
 
         ### mettere in attesa finche non si allontana (tramite laser e sonar)        
 
-        anyTouch.signal.disconnect(idAnyTouch)
+        anyTouch.signal.disconnect(idAnyTouch)    
 
-        tm = int(time.time())
         im.robot.memory_service.insertData('Device/SubDeviceList/Platform/Front/Sonar/Sensor/Value', 0.0)
         im.robot.memory_service.insertData('Device/SubDeviceList/Platform/Back/Sonar/Sensor/Value', 0.0)
 
