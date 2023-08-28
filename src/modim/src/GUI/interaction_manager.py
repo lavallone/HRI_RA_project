c406081eeac06035d62b075a5566fe0a1fb5c877
AlessiaCiarlo <ciarlo.1796690@studenti.uniroma1.it>
2023-08-28 13:04:00 +0200

    Game gestures

--- src/modim/src/GUI/interaction_manager.py
+++ src/modim/src/GUI/interaction_manager.py
@@ -5,6 +5,7 @@ import os
 import time, datetime
 import socket
 import random
+import threading
 
 from actionReader import *
 from actionWriter import ActionWriter
@@ -407,7 +408,8 @@ class InteractionManager:
             if ans == class_wast: 
                 c += 1
                 ########## GESTURE ##########
-                self.robot.yes('elementary')
+                thread = threading.Thread(target = lambda: self.robot.yes(age = "elementary")) 
+                thread.start()
                 #############################
                 self.execute('correct')
                 time.sleep(3)
@@ -416,7 +418,8 @@ class InteractionManager:
                 path_waste = dir_path+img_waste
             else: 
                 ########## GESTURE ##########
-                self.robot.no('elementary')
+                thread = threading.Thread(target = lambda: self.robot.no(age = "elementary")) 
+                thread.start()
                 #############################
                 self.execute('wrong')
                 time.sleep(3)
